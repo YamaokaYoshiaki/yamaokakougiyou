1441d767789e463b3429449bd275b6269775cfaf
山岡　義明 <1435091t@istc-a-062.epc.center.kobe-u.ac.jp>
2017-04-12 15:14:52 +0900

    Initial commit

--- W02/vec3.js
+++ W02/vec3.js
@@ -50,17 +50,11 @@ vec3.prototype.mid = function(v){
 vec3.prototype.triangle = function(v1,v2,v3){
     var x0=v1.x;
     var y0=v1.y;
-    var z0=v1.z;
-    var x1=v2.x;
-    var y1=v2.y;
-    var z1=v2.z;
-    var x2=v3.x;
-    var y2=v3.y;
-    var z2=v3.z;
-    var v0=new vec3(x0,y0,z0);
-    var v1=new vec3(x1,y1,z1);
-    var v2=new vec3(x2,y2,z2);
-    var t = (x0*y1*z2+y0*z1*x2+z0*x1*y2-x0*z1*y2-y0*x1*z2-z0*y1*x2)/2
+    var x1=v2.x-x0;
+    var y1=v2.y-y0;
+    var x2=v3.x-x0;
+    var y2=v3.y-y0;
+    var t = (x1*y2-x2*y1)/2
     return t;
     
 }
