01778793d17455cb45176c0c762d2043aaa94cb4
yamaokayoshiaki <yamaoka@yamaokayoshiakinoiMac.local>
2017-04-14 15:55:12 +0900

    Initial commit

--- W02/vec3.js
+++ W02/vec3.js
@@ -1,63 +1,98 @@
-vec3 = function(x,y,z)
+// Constructor
+Vec3 = function( x, y, z )
 {
     this.x = x;
     this.y = y;
     this.z = z;
 }
-vec3.prototype.max = function(v){
-    var x=v.x;
-    var y=v.y;
-    var z=v.z;
-    if(x>y){
-	if(x>z)
-	    return x;
-	else
-	    return z;
-    }else{
-	if(y>z)
-	    return y;
-	else
-	    return z;
+
+Vec3.prototype.max=function()
+{
+    var max=0;
+
+    if(this.x>this.y)
+    {
+    max=this.x;
     }
-}
-
-vec3.prototype.min = function(v){
-    var x=v.x;
-    var y=v.y;
-    var z=v.z;
-
-    if(x<y){
-	if(x<z)
-	    return x;
-	else
-	    return z;
-    }else{
-	if(y<z)
-	    return y;
-	else
-	    return z;
+    else
+    {
+    max=this.y;
     }
+    if(this.z>max)
+    {
+    max=this.z;
+    }
+    return max;
 }
 
-vec3.prototype.mid = function(v){
-    var x=v.x;
-    var y=v.y;
-    var z=v.z;
-
-    return x+y+z-this.min(v)-this.max(v);
+Vec3.prototype.min=function()
+{
+    var min=0;
+    if(this.x>this.y)
+    {
+    min=this.y;
+    }
+    else
+    {
+    min=this.x;
+    }
+    if(min>this.z)
+    {
+    min=this.z;
+    }
+    return min;
 }
 
-vec3.prototype.triangle = function(v1,v2,v3){
-    var x0=v1.x;
-    var y0=v1.y;
-    var x1=v2.x-x0;
-    var y1=v2.y-y0;
-    var x2=v3.x-x0;
-    var y2=v3.y-y0;
-    var t = (x1*y2-x2*y1)/2
-    if(t<0)
-	t*=-1
-    return t;
-    
+Vec3.prototype.mid=function()
+{
+    var mid=0;
+    var sum=0;
+    sum=this.x+this.y+this.z;
+    mid=sum-this.max()-this.min();
+    return mid;
 }
 
+
+
+Vec3.prototype.minus = function(v)
+
+{
+
+this.x=v.x-this.x;
+
+this.y=v.y-this.y;
+
+this.z=v.z-this.z;
+
+return this;
+
+}
+
+Vec3.prototype.CrossProduct = function(v)
+{
+    this.x=v.y*this.z-this.y*v.z;
+    this.y=v.z*this.x-this.z*v.x;
+    this.z=v.x*this.y-this.x*v.y;
+    return this;
+}
+
+Vec3.prototype.Length=function();
+{
+    var length=0;
+length=Math.pow(this.x,2)+Math.pow(this.y,2)+Math.pow(this.z,2);
+    length=Math.pow(length,1/2);
+    return length;
+}
+
+Vec3.prototype.AreaTriangle()=function(v1,v2)
+{
+    var length=0;
+    var CP=0;
+    var Area=0;
+    v1=v1.minus(this);
+    v2=v2.minus(this);
+    CP=v1.CrossProduct(v2);
+    length=CP.Length();
+    Area=length/2;
+    return Area;
+}
