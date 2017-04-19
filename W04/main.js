4a40893b2805b658b88b29e6c25936d47f1c3ad4
山岡　義明 <1435091t@istc-a-050.epc.center.kobe-u.ac.jp>
2017-04-19 14:19:30 +0900

    Initial commit

--- W04/main.js
+++ W04/main.js
@@ -27,8 +27,8 @@ function main()
     function loop()
     {
         requestAnimationFrame( loop );
-        cube.rotation.x += 0.001;
-        cube.rotation.y += 0.001;
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
         renderer.render( scene, camera );
     }
 }
