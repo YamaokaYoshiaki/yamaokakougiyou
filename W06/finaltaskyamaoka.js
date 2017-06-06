ab41a04f18c6aa51a73f5e48e800c982b56515a6
山岡　義明 <1435091t@istc-a-070.epc.center.kobe-u.ac.jp>
2017-06-06 14:25:28 +0900

    Initial commit

--- W06/finaltaskyamaoka.js
+++ W06/finaltaskyamaoka.js
@@ -1,19 +1,16 @@
-function main()
+function main(type)
 {
     var volume = new KVS.LobsterData();
     var screen = new KVS.THREEScreen();
-
     screen.init( volume, {
         width: window.innerWidth,
-        height: window.innerHeight,
+        height: window.innerHeight/2.3,
         enableAutoResize: false	
     });
-  
     var bounds = Bounds( volume );
     screen.scene.add( bounds );
-        
     var isovalue = 128;
-    var surfaces = Isosurfaces( volume, isovalue, screen);
+    var surfaces = Isosurfaces( volume, isovalue, screen,type);
     screen.scene.add( surfaces );
 
     document.addEventListener( 'mousemove', function() {
@@ -23,6 +20,7 @@ function main()
     window.addEventListener( 'resize', function() {
         screen.resize( [ window.innerWidth, window.innerHeight ] );
     });
+    return screen;
 
-    screen.loop();
+
 }
